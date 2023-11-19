d272aef8a9204acabdf17390f86ccbe090b6b926
Predator654 <piotr.budzynski@protonmail.com>
2023-11-19 18:14:49 +0100

    Add js function for tile1 element to change color forth and back while clicked

--- movement.js
+++ movement.js
@@ -1,1 +1,8 @@
-
+function tileSwap() {
+    if (document.getElementById("tile1").style.backgroundColor === "red") {
+        document.getElementById("tile1").style.backgroundColor = "cadetblue";
+        document.getElementById("tile1:hover").style.backgroundColor = "#194F55";
+    } else {
+        document.getElementById("tile1").style.backgroundColor = "red";
+    }
+}
